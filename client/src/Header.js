2fcec34e139cbbbf2654a7aa78b014d65a345c39
abhay <abhaytxanax@gmail.com>
2023-06-07 15:08:18 +0530

    header

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,23 +1,23 @@
-import {Link} from "react-router-dom";
-import {useContext, useEffect, useState} from "react";
-import {UserContext} from "./UserContext";
+import { Link } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "./UserContext";
 
 export default function Header() {
-  const {setUserInfo,userInfo} = useContext(UserContext);
+  const { setUserInfo, userInfo } = useContext(UserContext);
   useEffect(() => {
-    fetch('http://localhost:4000/profile', {
-      credentials: 'include',
-    }).then(response => {
-      response.json().then(userInfo => {
+    fetch("http://localhost:4000/profile", {
+      credentials: "include",
+    }).then((response) => {
+      response.json().then((userInfo) => {
         setUserInfo(userInfo);
       });
     });
   }, []);
 
   function logout() {
-    fetch('http://localhost:4000/logout', {
-      credentials: 'include',
-      method: 'POST',
+    fetch("http://localhost:4000/logout", {
+      credentials: "include",
+      method: "POST",
     });
     setUserInfo(null);
   }
@@ -26,7 +26,9 @@ export default function Header() {
 
   return (
     <header>
-      <Link to="/" className="logo">MyBlog</Link>
+      <Link to="/" className="logo">
+        TCP
+      </Link>
       <nav>
         {username && (
           <>
