eeebfdbb4730eff5d190bd992d932a55214d244a
dejwid <dawid.paszko@gmail.com>
2024-12-16 22:18:27 +0100

    connection stirn got .env

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
-mongoose.connect('mongodb+srv://blog:RD8paskYC8Ayj09u@cluster0.pflplid.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect(process.env.MONGO_URI);
 
 app.post('/register', async (req,res) => {
   const {username,password} = req.body;
