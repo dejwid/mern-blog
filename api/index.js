ad9c5350098a70216535894d1f4c8cdfcc478370
dejwid <dawid.paszko@gmail.com>
2024-12-17 15:44:02 +0100

    AWS_* -> AWS_BUCKET_*

--- api/index.js
+++ api/index.js
@@ -15,8 +15,8 @@ require('dotenv').config();
 const s3 = new S3Client({
   region: 'us-east-1',
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_BUCKET_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_BUCKET_SECRET_ACCESS_KEY,
   },
 })
 
